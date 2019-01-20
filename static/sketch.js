5a69e326613a001c5edb7232d49ebcbb1043af07
Slavik <slavik.sukhanov@iskyweb.com>
2019-01-20 15:55:33 +0200

    feat: add loading of image data

--- static/sketch.js
+++ static/sketch.js
@@ -1,17 +1,39 @@
 (function () {
-    const img = document.getElementById('img');
-    const imgFile = document.getElementById('img-file');
+    const selectedImg = document.getElementById('selectedImg');
+    const imgFile = document.getElementById('imgFile');
+    const predictBtn = document.getElementById('predictBtn');
+    const predictList = document.getElementById('predictList');
+    const predictionResult = document.getElementById('predictionResult');
 
-    // imgFile.addEventListener('change', function () {
-    //     const imgSrc
 
-    // });
-    
-    mobilenet.load().then(model => {
-        // Classify the image.
-        model.classify(img).then(predictions => {
-          console.log('Predictions: ');
-          console.log(predictions);
+    imgFile.addEventListener('change', function () {
+        const reader = new FileReader();
+        reader.onload = function() {
+            const dataURL = reader.result;
+            selectedImg.setAttribute('src', dataURL);
+        }
+
+        const file = imgFile.files[0];
+        reader.readAsDataURL(file);
+        predictBtn.disabled = false;
+    });
+
+    predictBtn.addEventListener('click', function () {
+        predictList.innerHTML = '';
+
+
+        mobilenet.load().then(model => {
+            model.classify(selectedImg).then(predictions => {
+                predictions.forEach((predict, index) => {
+                    const li = document.createElement('li');
+                    li.classList.add('list-group-item');
+                    li.innerHTML = `${index+1}. ${predict.className}: ${predict.probability}`;
+
+                    predictList.appendChild(li);
+                });
+
+                predictionResult.style.display = 'block';
+            });
         });
     });
 })();
